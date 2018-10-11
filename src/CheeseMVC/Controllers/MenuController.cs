1b0e94d90768da07385f2b2c895783d1e3b38793
DayStar24 <walters.ralph@outlook.com>
2018-10-11 11:56:49 -0500

    Part 3: Updated MenuController

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -30,5 +30,24 @@ namespace CheeseMVC.Controllers
 
             return View(addMenuViewModel);
         }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu");
+            }
+
+            return View(addMenuViewModel);
+        }
     }
 }
